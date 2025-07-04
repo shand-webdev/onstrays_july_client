fec68369e8567d3fba4d5ddd3737dcee43e981f0
shand-webdev <iamgowrishankar05@gmail.com>
2025-07-04 13:34:32 +0530

    added turn

--- src/App.js
+++ src/App.js
@@ -33,9 +33,19 @@ function App() {
       { urls: "stun:stun2.l.google.com:19302" },
       { urls: "stun:stun3.l.google.com:19302" },
       { urls: "stun:stun4.l.google.com:19302" },
-    ],
-    iceCandidatePoolSize: 10,
-  };
+     {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject", 
+      credential: "openrelayproject"
+    }
+  ],
+  iceCandidatePoolSize: 10,
+};
 
   // Create peer connection
   const createPeerConnection = useCallback(() => {
