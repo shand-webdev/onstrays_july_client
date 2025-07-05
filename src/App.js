4bba12ab9dac77d48d1eb6926379e8541a5c39a1
shand-webdev <iamgowrishankar05@gmail.com>
2025-07-05 20:15:22 +0530

    updated turn

--- src/App.js
+++ src/App.js
@@ -35,15 +35,21 @@ function App() {
       { urls: "stun:stun2.l.google.com:19302" },
       { urls: "stun:stun3.l.google.com:19302" },
       { urls: "stun:stun4.l.google.com:19302" },
-     {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject"
+    
+      {
+      urls: "turn:numb.viagenie.ca",
+      username: "webrtc@live.com",
+      credential: "muazkh"
     },
     {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject", 
-      credential: "openrelayproject"
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      username: "28224511:1379330808",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA="
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=tcp",
+      username: "28224511:1379330808", 
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA="
     }
   ],
   iceCandidatePoolSize: 10,
